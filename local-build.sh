abaf7f667c6df589e6006b98debf5d062a8573cf
q0s <37809416+babinkos@users.noreply.github.com>
2023-05-08 12:22:01 +0300

    local-build.sh : add arm64 support (multiarch : linux/amd64,linux/arm64)

--- local-build.sh
+++ local-build.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/env bash
-docker build --no-cache --progress plain -f Dockerfile -t 503110391064.dkr.ecr.eu-central-1.amazonaws.com/sign-svc:latest .
+# docker build --no-cache --progress plain -f Dockerfile -t 503110391064.dkr.ecr.eu-central-1.amazonaws.com/sign-svc:latest .
+docker buildx build --platform linux/amd64,linux/arm64 --push --no-cache --progress plain -f Dockerfile -t 503110391064.dkr.ecr.eu-central-1.amazonaws.com/sign-svc:latest .
+echo ""
+docker buildx imagetools inspect 503110391064.dkr.ecr.eu-central-1.amazonaws.com/sign-svc:latest
